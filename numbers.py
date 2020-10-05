869e04f53515f49eaf1974741562f291e5d2e05e
harishramuk <50021086+harishramuk@users.noreply.github.com>
2020-10-05 12:20:49 +0530

    initial commit

---
+++ numbers.py
@@ -0,0 +1,18 @@
+n = int(input('enter the no 1 to 100:'))
+output = ''
+word_ones = [' ','one','two','three','four',
+             'five','six','seven','eight','nine','ten',
+             'eleven','twelve','thirteen','fourteen',
+             'fifteen','sixteen','seventine','eighteen',
+             'ninteen']
+word_tens = ['','','twenty','thirty','fourty','fifty',
+             'sixty','seventy','eighty','ninty']
+if n ==0:
+    print('zero')
+elif n<=19:
+    print(word_ones[n])
+elif n<=100:
+    print((word_tens[n//10])+' '+(word_ones[n%10]))
+else:
+    print('n is not in bettwin for 1 to 100:')
+    
